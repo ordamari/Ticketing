a6defa11148b9aa8cb804ce1a7a1a70fb3f198a9
ordamari <ordamari971@gmail.com>
2023-06-08 19:07:21 +0300

    Test setup

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user.route";
+import { signinRouter } from "./routes/signin.route";
+import { signupRouter } from "./routes/signup.route";
+import { signoutRouter } from "./routes/signout.route";
+
+import { errorHandler } from "./middlewares/error-handler.middleware";
+import { NotFoundError } from "./errors/not-found.error";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user.route";
-import { signinRouter } from "./routes/signin.route";
-import { signupRouter } from "./routes/signup.route";
-import { signoutRouter } from "./routes/signout.route";
-
-import { errorHandler } from "./middlewares/error-handler.middleware";
-import { NotFoundError } from "./errors/not-found.error";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all("*", () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
