fdfaaa3541d2e3a344e827872ea67f9105af20c2
ordamari <ordamari971@gmail.com>
2023-06-17 20:37:12 +0300

    use common function from npm and start setup tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
